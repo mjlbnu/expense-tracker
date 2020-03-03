ee7b74a4a222f0b49ce91d1645ffcd897b1d8607
mjlbnu <mjlbnu@gmail.com>
2020-03-03 00:03:59 -0300

    getTransactions OK

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -1,20 +1,35 @@
+const Transaction = require('../models/Transaction');
+
 // @desc   Get all transactions
 // @route  GET /api/v1/transactions
 // @access Public
-exports.getTransactions = (req, res, next) => {
-  res.send('GET transactions');
+exports.getTransactions = async (req, res, next) => {
+  try {
+    const transactions = await Transaction.find();
+
+    return res.status(200).json({
+      success: true,
+      count: transactions.length,
+      data: transactions,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
 
 // @desc   Add transaction
 // @route  POST /api/v1/transactions
 // @access Public
-exports.addTransaction = (req, res, next) => {
+exports.addTransaction = async (req, res, next) => {
   res.send('POST transaction');
 };
 
 // @desc  Delete transaction
 // @route DELETE /api/v1/transaction/:id
 // @access Public
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
   res.send('DELETE transaction');
 };
