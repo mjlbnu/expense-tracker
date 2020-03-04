3c4e940109b62d9071ab87d4b5e13eb5e9625e8f
mjlbnu <mjlbnu@gmail.com>
2020-03-03 22:48:13 -0300

    deleteTransaction done

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -54,5 +54,26 @@ exports.addTransaction = async (req, res, next) => {
 // @route DELETE /api/v1/transaction/:id
 // @access Public
 exports.deleteTransaction = async (req, res, next) => {
-  res.send('DELETE transaction');
+  try {
+    const transaction = await Transaction.findById(req.params.id);
+
+    if (!transaction) {
+      return res.status(404).json({
+        success: false,
+        error: 'No transaction found',
+      });
+    }
+
+    await transaction.remove();
+
+    return res.status(200).json({
+      success: true,
+      data: {},
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: 'Server Error',
+    });
+  }
 };
