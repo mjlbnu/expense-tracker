9b366ad4be7427ac469e1a27b69172c430dd6289
mjlbnu <mjlbnu@gmail.com>
2020-03-09 23:31:32 -0300

    Transactions from context OK

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,13 @@ import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
 
+import { GlobalProvider } from './context/GlobalState';
+
 import './App.css';
 
 function App() {
   return (
-    <div>
+    <GlobalProvider>
       <Header />
       <div className="container">
         <Balance />
@@ -17,7 +19,7 @@ function App() {
         <TransactionList />
         <AddTransaction />
       </div>
-    </div>
+    </GlobalProvider>
   );
 }
 
