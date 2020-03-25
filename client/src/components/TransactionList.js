1703af72d4f43474757eef7a0f29e61ffcce70af
mjlbnu <mjlbnu@gmail.com>
2020-03-24 22:39:42 -0300

    getTransactions from database and morgan log

--- client/src/components/TransactionList.js
+++ client/src/components/TransactionList.js
@@ -1,10 +1,15 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Transaction } from './Transaction';
 
 import { GlobalContext } from '../context/GlobalState';
 
 export const TransactionList = () => {
-  const { transactions } = useContext(GlobalContext);
+  const { transactions, getTransactions } = useContext(GlobalContext);
+
+  useEffect(() => {
+    getTransactions();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
--- server.js
+++ server.js
@@ -15,6 +15,10 @@ const app = express();
 // allow to use bodyparser
 app.use(express.json());
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
